fa2e10a956b563428c55f2a21206dbd8118d1f0d
Junior Safera <juniorsafera.dev@gmail.com>
2022-03-09 03:32:38 -0300

    Create form_cadastro.dart

---
+++ beaba_produtos_teste_conhecimento_flutter/lib/components/form_cadastro.dart
@@ -0,0 +1,226 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:math';
+
+import 'package:beaba_produtos_teste_conhecimento_flutter/model/modelFuncionario.dart';
+import 'package:beaba_produtos_teste_conhecimento_flutter/provider/provider_funcionarios.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+
+class FormCadastro extends StatefulWidget {
+  final ModelFuncionario funcionario;
+  const FormCadastro({Key? key, required this.funcionario}) : super(key: key);
+
+  @override
+  State<FormCadastro> createState() => _FormCadastroState();
+}
+
+final _form = GlobalKey<FormState>();
+
+DateTime? _dataSelecionada;
+final TextEditingController cNome = TextEditingController();
+final TextEditingController cCargo = TextEditingController();
+final TextEditingController cSetor = TextEditingController();
+
+final Map<String, String> _dadosForm = {};
+
+void _carregarDados(ModelFuncionario funcionario) {
+  _dadosForm['id'] = funcionario.id;
+  _dadosForm['nome'] = funcionario.nome;
+  _dadosForm['cargo'] = funcionario.cargo;
+  _dadosForm['setor'] = funcionario.setor;
+}
+
+final funcionario = ModelFuncionario(
+    id: Random.secure().toString(),
+    nome: cNome.text,
+    cargo: cCargo.text,
+    setor: cSetor.text,
+    dataNascimento: _dataSelecionada!,
+    dataContratacao: DateTime.now());
+
+class _FormCadastroState extends State<FormCadastro> {
+  @override
+  Widget build(BuildContext context) {
+    final providerFuncionarios = Provider.of<FuncionariosProvider>(context);
+    final _funcionario =
+        ModalRoute.of(context)!.settings.arguments as ModelFuncionario;
+    _carregarDados(_funcionario);
+
+    _abrirCalendario() {
+      showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(1970),
+        lastDate: DateTime.now(),
+      ).then((value) {
+        if (value == null) {
+          return;
+        }
+        setState(() {
+          _dataSelecionada = value;
+        });
+      });
+    }
+
+    void validarCampos() {
+      if (cNome.text.isEmpty) {
+        showDialog<void>(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+            title: const Text('Atenção'),
+            content: const Text('Informe o nome do funcionário!'),
+            actions: <Widget>[
+              // ignore: deprecated_member_use
+              FlatButton(
+                child: const Text('Ok'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        );
+      } else if (cCargo.text.isEmpty) {
+        showDialog<void>(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+            title: const Text('Atenção'),
+            content: const Text('Informe o cargo do funcionário!'),
+            actions: <Widget>[
+              // ignore: deprecated_member_use
+              FlatButton(
+                child: const Text('Ok'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        );
+      } else if (cSetor.text.isEmpty) {
+        showDialog<void>(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+            title: const Text('Atenção'),
+            content: const Text('Informe o setor do funcionário!'),
+            actions: <Widget>[
+              // ignore: deprecated_member_use
+              FlatButton(
+                child: const Text('Ok'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        );
+      } else if (_dataSelecionada == null) {
+        showDialog<void>(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+            title: const Text('Atenção'),
+            content: const Text('Informe a data de Nascimento!'),
+            actions: <Widget>[
+              // ignore: deprecated_member_use
+              FlatButton(
+                child: const Text('Ok'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        );
+      } else {
+        providerFuncionarios.put(funcionario);
+      }
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Funcionário'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {
+                _form.currentState!.save();
+                Navigator.of(context).pop();
+              },
+              icon: Icon(Icons.save))
+        ],
+      ),
+      body: Container(
+        // ignore: prefer_const_literals_to_create_immutables
+        padding: EdgeInsets.all(15),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                controller: cNome,
+                // initialValue: _dadosForm['nome'],
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(labelText: 'Nome Completo'),
+              ),
+              TextFormField(
+                controller: cCargo,
+                // initialValue: _dadosForm['cargo'],
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(labelText: 'Cargo'),
+              ),
+              TextFormField(
+                controller: cSetor,
+                // initialValue: _dadosForm['setor'],
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(labelText: 'Setor'),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    FlatButton(
+                      onPressed: () {
+                        _abrirCalendario();
+                      },
+                      child: Text('Data Nascimento: '),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    _dataSelecionada == null
+                        ? Text('')
+                        : Text(
+                            DateFormat('dd/MM/y').format(_dataSelecionada!),
+                          ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    RaisedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Text('Cancelar'),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text('Salvar'),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
