13c33873c63b181232a7413e182f9d96b66c9535
Junior Safera <juniorsafera.dev@gmail.com>
2022-03-09 03:32:32 -0300

    Create dados.dart

---
+++ beaba_produtos_teste_conhecimento_flutter/lib/data/dados.dart
@@ -0,0 +1,28 @@
+import 'package:beaba_produtos_teste_conhecimento_flutter/model/modelFuncionario.dart';
+
+Map listaFuncionarios = {
+  '1': ModelFuncionario(
+    id: '1',
+    nome: 'Maria',
+    cargo: 'Secretária',
+    setor: 'Educção',
+    dataNascimento: DateTime.now(),
+    dataContratacao: DateTime.now(),
+  ),
+  '2': ModelFuncionario(
+    id: '2',
+    nome: 'Juliete',
+    cargo: 'Secretária',
+    setor: 'Educção',
+    dataNascimento: DateTime.now(),
+    dataContratacao: DateTime.now(),
+  ),
+  '3': ModelFuncionario(
+    id: '3',
+    nome: 'Tateu',
+    cargo: 'Secretária',
+    setor: 'Educção',
+    dataNascimento: DateTime.now(),
+    dataContratacao: DateTime.now(),
+  ),
+};
