8ee923a29bd6393ce6250ff2edd8d383cb7cdfe1
Junior Safera <juniorsafera.dev@gmail.com>
2022-03-09 22:33:10 -0300

    Update provider_funcionarios.dart

--- beaba_produtos_teste_conhecimento_flutter/lib/provider/provider_funcionarios.dart
+++ beaba_produtos_teste_conhecimento_flutter/lib/provider/provider_funcionarios.dart
@@ -9,7 +9,6 @@ import 'package:flutter/cupertino.dart';
 
 class FuncionariosProvider with ChangeNotifier {
   final Map<String, ModelFuncionario> _funcionarios = {...listaFuncionarios};
-  
 
   List<ModelFuncionario> get funcionariosCadastrados =>
       [..._funcionarios.values];
@@ -18,38 +17,22 @@ class FuncionariosProvider with ChangeNotifier {
     return _funcionarios.length;
   }
 
-  void put(ModelFuncionario funcionario) {
-    if (funcionario == null) {
-      return;
-    }
+  void cadastrar(ModelFuncionario funcionario) {
+    // adicionar
+    final id = Random().nextDouble().toString();
+    _funcionarios.putIfAbsent(
+      id,
+      () => ModelFuncionario(
+        id: id,
+        nome: funcionario.nome,
+        cargo: funcionario.cargo,
+        setor: funcionario.setor,
+        dataNascimento: funcionario.dataNascimento,
+        dataContratacao: DateTime.now(),
+        demitido: false,
+      ),
+    );
 
-    if (funcionario.id != null &&
-        funcionario.id.trim().isEmpty &&
-        _funcionarios.containsKey(funcionario.id)) {
-      _funcionarios.update(
-          funcionario.id,
-          (_) => ModelFuncionario(
-              id: funcionario.id,
-              nome: funcionario.nome,
-              cargo: funcionario.cargo,
-              setor: funcionario.setor,
-              dataNascimento: funcionario.dataNascimento,
-              dataContratacao: funcionario.dataContratacao));
-    } else {
-      // adicionar
-      final id = Random().nextDouble().toString();
-      _funcionarios.putIfAbsent(
-        id,
-        () => ModelFuncionario(
-          id: id,
-          nome: funcionario.nome,
-          cargo: funcionario.cargo,
-          setor: funcionario.setor,
-          dataNascimento: funcionario.dataNascimento,
-          dataContratacao: DateTime.now(),
-        ),
-      );
-    }
     notifyListeners();
   }
 
@@ -65,12 +48,15 @@ class FuncionariosProvider with ChangeNotifier {
     _funcionarios.update(
       funcionario.id,
       (_) => ModelFuncionario(
-          id: funcionario.id,
-          nome: funcionario.nome,
-          cargo: funcionario.cargo,
-          setor: funcionario.setor,
-          dataNascimento: funcionario.dataNascimento,
-          dataContratacao: funcionario.dataContratacao),
+        id: funcionario.id,
+        nome: funcionario.nome,
+        cargo: funcionario.cargo,
+        setor: funcionario.setor,
+        dataNascimento: funcionario.dataNascimento,
+        dataContratacao: funcionario.dataContratacao,
+        demitido: funcionario.demitido,
+        dataDemissao: funcionario.dataDemissao,
+      ),
     );
     notifyListeners();
   }
